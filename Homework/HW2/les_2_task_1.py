dba7442a29fb35d4e831d66e1eb90e58fdb6aeaa
Dany Sat <dany_sat@mail.ru>
2020-01-28 21:19:31 +0300

    HW
    сорри, забыл про домашку, вот залил

--- Homework/HW2/les_2_task_1.py
+++ Homework/HW2/les_2_task_1.py
@@ -1,2 +1,9 @@
-
-
+a = input('Введите число яблок в корзине "a":')
+b = input('Введите число яблок в корзине "b":')
+print('a is ' + a)
+print('b is ' + b)
+c = a
+a = b
+b = c
+print('New a is ' + a)
+print('New b is ' + b)
