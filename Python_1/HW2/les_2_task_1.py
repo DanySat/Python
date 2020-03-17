9bcfa0c6e7a69e9e9f93a23d8357e1c20b8df57c
Dany <Dany_sat@mail.ru>
2020-03-17 22:05:38 +0300

    123

--- Python_1/HW2/les_2_task_1.py
+++
@@ -1,9 +0,0 @@
-a = input('Введите число яблок в корзине "a":')
-b = input('Введите число яблок в корзине "b":')
-print('a is ' + a)
-print('b is ' + b)
-c = a
-a = b
-b = c
-print('New a is ' + a)
-print('New b is ' + b)
